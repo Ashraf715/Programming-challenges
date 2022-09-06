bffa9f6d04bbc325d7c68c058b0e08ac367e482d
Ashraf715 <112864748+Ashraf715@users.noreply.github.com>
2022-09-06 10:32:22 +0100

    Add files via upload

---
+++ 1/challenge1.py
@@ -0,0 +1,14 @@
+rods = float(input ("Enter the number of rods."))
+#conversions begin here
+meters = rods*5.0292
+feet = rods*5.0292/0.3048
+miles = rods*5.0292/1609.34
+furlongs = rods/40
+mtw = rods*3.1/60
+print ("You entered", rods, "rods")
+print ("Here are the conversions:")
+print ("Meters:", meters)
+print ("Feet:", feet)
+print ("Miles:", miles)
+print ("Furlongs:", furlongs)
+print ("Minutes to walk", rods, "rods", mtw)
