7aad4e9fcd54e151db444f55b1ef0b079ae0a269
Ashraf715 <112864748+Ashraf715@users.noreply.github.com>
2022-09-07 12:37:16 +0100

    Update challenge1.py

--- 1/challenge1.py
+++ 1/challenge1.py
@@ -1,13 +1,23 @@
 rods = float(input ("Enter the number of rods."))
-meters = rods*5.0292
-feet = rods*5.0292/0.3048
-miles = rods*5.0292/1609.34
-furlongs = rods/40
-mtw = rods*3.1/60
-print ("You entered", rods, "rods")
+def rods_to_meters(rods):
+    return rods*5.0292
+
+def rods_to_feet(rods):
+    return rods*5.0292/0.3048
+
+def rods_to_miles(rods):
+    return rods*5.0292/1609.34
+
+def rods_to_furlongs(rods):
+    return rods/40
+
+def rods_to_mtw(rods):
+    return rods*3.1/60
+
+print (f"You entered {rods} rods")
 print ("Here are the conversions:")
-print ("Meters:", meters)
-print ("Feet:", feet)
-print ("Miles:", miles)
-print ("Furlongs:", furlongs)
-print ("Minutes to walk", rods, "rods", mtw)
+print ("Meters:", rods_to_meters(rods))
+print ("Feet:", rods_to_feet(rods))
+print ("Miles:", rods_to_miles(rods))
+print ("Furlongs:", rods_to_furlongs(rods))
+print (f"Minutes to walk {rods} rods:", rods_to_mtw(rods))
